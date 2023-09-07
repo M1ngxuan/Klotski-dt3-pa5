2ef6443d696000ad089036fb1280b50d4e388fbe
jiaqin hong <118160486+hjq0127@users.noreply.github.com>
2023-09-07 17:46:54 +0800

    Update DataHelper.java

--- app/src/main/java/com/example/newklotski/DataHelper.java
+++ app/src/main/java/com/example/newklotski/DataHelper.java
@@ -8,21 +8,26 @@ public class DataHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "Klotski.db";
     public static final int DATABASE_VERSION = 1;
+
     public DataHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
                       int version) {
-        //The constructor in the parent class must be called through super
+        // The constructor in the parent class must be called through super
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "create table user(id integer primary key autoincrement,game_name varchar(64),steps Integer)";
+        // Called when the database is created for the first time
+        // Create a table named "user" with columns: id (auto-incrementing integer), game_name (text), steps (integer)
+        String sql = "CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT, game_name VARCHAR(64), steps INTEGER)";
         db.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String sql = "alter table user add sex varchar(8)";
+        // Called when the database needs to be upgraded to a new version
+        // Add a new column named "sex" (text) to the "user" table
+        String sql = "ALTER TABLE user ADD COLUMN sex VARCHAR(8)";
         db.execSQL(sql);
     }
 }
