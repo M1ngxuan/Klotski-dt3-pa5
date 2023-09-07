e990dbb1d3b57e77630c9c5f07aa7b2a92c5f572
MingYu_Zhao <96897237+ArthurMY@users.noreply.github.com>
2023-09-07 17:15:18 +0800

    Update RankActivity.java

--- app/src/main/java/com/example/newklotski/RankActivity.java
+++ app/src/main/java/com/example/newklotski/RankActivity.java
@@ -1,3 +1,5 @@
+// This class represents the RankActivity, which displays a list of ranked game levels
+// and the number of steps taken to complete each level.
 package com.example.newklotski;
 
 import android.database.Cursor;
@@ -11,9 +13,9 @@ import java.util.ArrayList;
 
 public class RankActivity extends AppCompatActivity {
 
-    private ListView listView;
-    private ArrayList<String> rankList = new ArrayList<>();
-    private ArrayAdapter<String> adapter;
+    private ListView listView; // ListView to display the ranked game levels
+    private ArrayList<String> rankList = new ArrayList<>(); // List to store ranked game data
+    private ArrayAdapter<String> adapter; // Adapter to connect the list data to the ListView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +24,27 @@ public class RankActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.rank_list);
 
-        // Set up the ArrayAdapter
+        // Set up the ArrayAdapter to display the rankList data in the ListView
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, rankList);
         listView.setAdapter(adapter);
 
+        // Load ranked game data from the database and populate the rankList
         loadData();
     }
 
+    // This method retrieves ranked game data from the database and populates the rankList.
     private void loadData() {
         DataHelper dbHelper = new DataHelper(this, DataHelper.DATABASE_NAME, null, 1);
         SQLiteDatabase sqliteDatabase = dbHelper.getReadableDatabase();
 
+        // Query the database to retrieve game_name and steps, ordered by game_name and steps.
         Cursor cursor = sqliteDatabase.rawQuery("SELECT game_name, steps FROM user ORDER BY game_name ASC, steps ASC", null);
 
         while (cursor.moveToNext()) {
             String gameName = cursor.getString(cursor.getColumnIndex("game_name"));
             int steps = cursor.getInt(cursor.getColumnIndex("steps"));
 
-            // Format the game level
+             // Format the game level and steps, then add it to rankList.
             String formattedGameName = "Level " + (Integer.parseInt(gameName) + 1);
 
             rankList.add(formattedGameName + ": " + steps + " steps");
@@ -47,7 +52,7 @@ public class RankActivity extends AppCompatActivity {
         cursor.close();
         sqliteDatabase.close();
 
-        // Notify the adapter that data has changed
+        // Notify the adapter that data has changed so it updates the ListView.
         adapter.notifyDataSetChanged();
     }
 
