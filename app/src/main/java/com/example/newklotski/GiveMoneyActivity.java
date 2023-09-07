3ec1554af945a0a126d4ad51aec14f2cebb76025
MingYu_Zhao <96897237+ArthurMY@users.noreply.github.com>
2023-09-07 17:47:06 +0800

    Update GiveMoneyActivity.java

--- app/src/main/java/com/example/newklotski/GiveMoneyActivity.java
+++ app/src/main/java/com/example/newklotski/GiveMoneyActivity.java
@@ -1,3 +1,8 @@
+/**
+ * This class represents the GiveMoneyActivity, which is an activity in the Android app.
+ * It is responsible for displaying the user interface related to giving money or donations.
+ * The activity extends AppCompatActivity and overrides the onCreate method to set the content view.
+ */
 package com.example.newklotski;
 
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +13,6 @@ public class GiveMoneyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_give_money);
+        setContentView(R.layout.activity_give_money); // Set the layout for this activity
     }
 }
