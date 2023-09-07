73c15d5745d4ca42bb50a549d4296a7372759a80
jiaqin hong <118160486+hjq0127@users.noreply.github.com>
2023-09-07 17:45:35 +0800

    Update ContactUsActivity.java

--- app/src/main/java/com/example/newklotski/ContactUsActivity.java
+++ app/src/main/java/com/example/newklotski/ContactUsActivity.java
@@ -15,24 +15,31 @@ public class ContactUsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact_us);
     }
 
-    public void return_contact(View v) {
-        Intent intent = new Intent(ContactUsActivity.this,MainActivity.class);
+  public void return_contact(View v) {
+        // Create an intent to start the MainActivity
+        Intent intent = new Intent(ContactUsActivity.this, MainActivity.class);
+        // Start the MainActivity
         startActivity(intent);
     }
 
-    public void send_contact(View v) {
-        final AlertDialog.Builder alterDialog  = new AlertDialog.Builder(ContactUsActivity.this);
+public void send_contact(View v) {
+        // Create an AlertDialog.Builder object
+        final AlertDialog.Builder alterDialog = new AlertDialog.Builder(ContactUsActivity.this);
+        // Set the title of the dialog
         alterDialog.setTitle("Success Send");
+        // Set a negative button "OK" with an OnClickListener
         alterDialog.setNegativeButton("OK",
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        // Dismiss the dialog
                         dialog.dismiss();
-                        Intent intent = new Intent(ContactUsActivity.this,MainActivity.class);
+                        // Create an intent to start the MainActivity
+                        Intent intent = new Intent(ContactUsActivity.this, MainActivity.class);
+                        // Start the MainActivity
                         startActivity(intent);
                     }
                 });
+        // Create and show the AlertDialog
         alterDialog.create().show();
     }
-
-}
